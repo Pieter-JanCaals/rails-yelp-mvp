779478791f1d6cf62dd4ad3b2fc6b5dc9e3924bf
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-13 11:11:35 -0500

    created validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :restaurant, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: 0..5, numericality: {only_integer: true}
 end
