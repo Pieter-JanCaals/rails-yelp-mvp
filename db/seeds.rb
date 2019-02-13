3bd6060692c1584f788a62d77ff98f321a03d873
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-13 14:45:54 -0500

    Finished review CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,13 @@
 CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
 
 5.times do
-  Restaurant.create(name: Faker::Restaurant.name,
-                    address: Faker::Address.street_address,
-                    phone_number: Faker::PhoneNumber.phone_number,
-                    category: CATEGORIES.sample)
+  restaurant = Restaurant.create( name: Faker::Restaurant.name,
+                                  address: Faker::Address.street_address,
+                                  phone_number: Faker::PhoneNumber.phone_number,
+                                  category: CATEGORIES.sample)
+  20.times do
+    Review.create(content: Faker::ChuckNorris.fact, rating: (0..5).to_a.sample, restaurant: restaurant)
+  end
 end
+
+
